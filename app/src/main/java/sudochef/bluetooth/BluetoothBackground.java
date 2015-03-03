7b3237bf68f7f8f4f8f88b10ba988a69fd148607
sudochef <sudochef@gmail.com>
2015-03-03 14:09:10 -0500

    start of bluetooth class for background

---
+++ app/src/main/java/sudochef/bluetooth/BluetoothBackground.java
@@ -0,0 +1,107 @@
+
+/**package sudochef.bluetooth;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.InputStream;
+import java.util.UUID;
+
+import sdp.sudochef.R;
+public class BluetoothBackground {
+    private BluetoothAdapter btAdapter = null;
+    private BluetoothSocket btSocket = null;
+    private OutputStream outStream = null;
+
+    private static final UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+
+    btAdapter = BluetoothAdapter.getDefaultAdapter();
+    checkBTState();
+
+}
+    @Override
+    public void onResume() {
+        super.onResume();
+        // connection methods are best here in case program goes into the background etc
+
+        //Get MAC address from DeviceListActivity
+        Intent intent = getIntent();
+        newAddress = intent.getStringExtra(DeviceListActivity.EXTRA_DEVICE_ADDRESS);
+
+        // Set up a pointer to the remote device using its address.
+        BluetoothDevice device = btAdapter.getRemoteDevice(newAddress);
+
+        //Attempt to create a bluetooth socket for comms
+        try {
+            btSocket = device.createRfcommSocketToServiceRecord(MY_UUID);
+        } catch (IOException e1) {
+            Toast.makeText(getBaseContext(), "ERROR - Could not create Bluetooth socket", Toast.LENGTH_SHORT).show();
+        }
+
+        // Establish the connection.
+        try {
+            btSocket.connect();
+        } catch (IOException e) {
+            try {
+                btSocket.close();        //If IO exception occurs attempt to close socket
+            } catch (IOException e2) {
+                Toast.makeText(getBaseContext(), "ERROR - Could not close Bluetooth socket", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        // Create a data stream so we can talk to the device
+        try {
+            outStream = btSocket.getOutputStream();
+        } catch (IOException e) {
+            Toast.makeText(getBaseContext(), "ERROR - Could not create bluetooth outstream", Toast.LENGTH_SHORT).show();
+        }
+        //When activity is resumed, attempt to send a piece of junk data ('x') so that it will fail if not connected
+        // i.e don't wait for a user to press button to recognise connection failure
+        sendData("x");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        //Pausing can be the end of an app if the device kills it or the user doesn't open it again
+        //close all connections so resources are not wasted
+
+        //Close BT socket to device
+        try     {
+            btSocket.close();
+        } catch (IOException e2) {
+            Toast.makeText(getBaseContext(), "ERROR - Failed to close Bluetooth socket", Toast.LENGTH_SHORT).show();
+        }
+    }
+    private void checkBTState() {
+        // Check device has Bluetooth and that it is turned on
+        if(btAdapter==null) {
+            Toast.makeText(getBaseContext(), "ERROR - Device does not support bluetooth", Toast.LENGTH_SHORT).show();
+            finish();
+        } else {
+            if (btAdapter.isEnabled()) {
+            } else {
+                //Prompt user to turn on Bluetooth
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, 1);
+            }
+        }
+    }
+    public void Preheat(int Temp){
+
+    }
+    public void timer(int Time){
+
+    }
+    public void hotplate(int Temp){
+
+    }
