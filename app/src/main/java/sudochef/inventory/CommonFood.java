2d8958b4ed08a5f6ccfc5b99b9014dc6e639032d
Eli Siskind <esiskind@umass.edu>
2015-04-02 12:34:59 -0400

    Changed enums to use two word strings

--- app/src/main/java/sudochef/inventory/CommonFood.java
+++ app/src/main/java/sudochef/inventory/CommonFood.java
@@ -4,23 +4,38 @@ package sudochef.inventory;
  * Created by LH-21 on 4/1/2015.
  */
 public enum CommonFood {
-    Apple,
-    Banana,
-    Milk,
-    Mayonnaise,
-    Margarine,
-    Salt, //House
-    Butter,
-    Flour;
+    Apple("Apple"),
+    BakingSoda("Baking Soda"),
+    Banana("Banana"),
+    Milk("Milk"),
+    Mayonnaise("Mayonnaise"),
+    Margarine("Shitty Butter"),
+    Salt("Salt"), //House
+    Butter("Butter"),
+    Flour("Flour"),
+    BakingPowder("Baking Powder"),
+    CocoaPowder("Cocoa Powder");
+
 
     public static String[] names() {
         CommonFood[] food = values();
         String[] names = new String[food.length];
 
         for (int i = 0; i < food.length; i++) {
-            names[i] = food[i].name();
+            names[i] = food[i].displayName();
         }
 
         return names;
     }
+
+    String displayName;
+
+    private CommonFood(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String displayName() {
+        return displayName;
+    }
+
 }
